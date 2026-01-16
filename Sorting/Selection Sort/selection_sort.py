fe47bc94b5518f377ba2fc3476e9584373d5c3f9
Satyadarsh verma <satya12darsh@gmail.com>
2026-01-16 16:24:25 +0530

    selection sort

---
+++ Sorting/Selection Sort/selection_sort.py
@@ -0,0 +1,11 @@
+arr = [2,3,1,0,6,5]
+
+for i in range(len(arr)):
+    index = i
+
+    for j in range(i+1, len(arr)):
+        if arr[j] < arr[index]:
+            index = j 
+
+            arr[i], arr[index]= arr[index] , arr[i] 
+print(arr)     
