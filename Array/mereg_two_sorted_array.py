5ccb7d29aa272ee54a94ce2ff5ede2738fdc5fb6
Satyadarsh verma <satya12darsh@gmail.com>
2026-01-22 11:35:56 +0530

    updated code

--- Array/mereg_two_sorted_array.py
+++ Array/mereg_two_sorted_array.py
@@ -10,26 +10,29 @@ result = []
 
 while i < n and j < m:
     if nums1[i] == nums2[j]:
-        if not  result or result[-1] != nums1[i]:
+        if not result or result[-1] != nums1[i]:
             result.append(nums1[i])
         i += 1
         j += 1
+
     elif nums1[i] < nums2[j]:
         if not result or result[-1] != nums1[i]:
             result.append(nums1[i])
         i += 1
+
     else:
         if not result or result[-1] != nums2[j]:
             result.append(nums2[j])
         j += 1
 
+
 while i < n:
-    if result[-1] != nums1[i]:
+    if not result or result[-1] != nums1[i]:
         result.append(nums1[i])
     i += 1
 
 while j < m:
-    if result[-1] != nums2[j]:
+    if not result or result[-1] != nums2[j]:
         result.append(nums2[j])
     j += 1
 
